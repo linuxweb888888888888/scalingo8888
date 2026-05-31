42aa3a4bf8d5a45e253fc13b05ccf91882c72d7f
linuxweb888888888888 <web88888888888888+linux88888888@gmail.com>
2026-05-31 18:46:10 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,7 +27,7 @@ const config = {
     restHost: 'api.hbdm.com',
     wsHost: 'wss://api.hbdm.com/linear-swap-ws',
     accounts: apiAccounts,
-    baseVolume: parseInt(process.env.BASE_VOLUME) || 100,
+    baseVolume: parseInt(process.env.BASE_VOLUME) || 1,
     winLossRatio: 1.5,
     maxStartSpread: 0.15,
     autoClosePct: 110,
