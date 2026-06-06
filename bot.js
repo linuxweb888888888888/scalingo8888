2ff42190d885a85e8fa6b36e41c4efadcf361b12
linuxweb888888888888 <web88888888888888+linux88888888@gmail.com>
2026-06-06 19:37:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,7 +12,7 @@ const BASE_URL = "https://api.crypto.games/v1";
 const DEFAULTS = {
     coin: "BTC",
     multiplier: 1.2,          
-    payout: 2.0,              
+    payout: 1.7,              
     balanceStep: 0.00000050,  
     betIncrement: 0.00000001  
 };
